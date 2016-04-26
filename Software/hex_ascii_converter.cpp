495333e3441bc479c7d0235daa9221f5b988dbf5
Candace Ross <ccross@mit.edu>
2016-04-26 00:26:14 -0400

    removed an commented unneeded function in hex to ascii

--- Software/hex_ascii_converter.cpp
+++ Software/hex_ascii_converter.cpp
@@ -3,14 +3,6 @@
 #include <fstream>
 using namespace std;
 
-//---- define conversion functions -----//
-// char convertCharToHex(char character){
-//   cout << "got char " << character << endl;
-//   char a;
-//   a << hex << character;
-//   cout << " and " << a;
-// }
-
 int main(){
   //define Speck specific numbers
   int num_digits_per_word = 6; //word size is 24, block size=48; each word therefore 6 hex digits; two words per line
@@ -28,7 +20,6 @@ int main(){
     while (getline(infile,line)){
       for (int char_index=0; char_index < line.length(); char_index++){
 	digit_counter = digit_counter + 1;
-	//cout << "counter" << digit_counter;
 	if (digit_counter!= 6 && digit_counter!=12){
 	  outfile << hex << (int)line[char_index]; // regular character write
 	}else if (digit_counter == 6){
@@ -37,9 +28,6 @@ int main(){
 	  outfile << hex << (int)line[char_index] << "\n"; // start writing on new line
 	  digit_counter = 0;
 	}
-      }
-      if (true){
-	return 0;
       }
     }
   }else{
