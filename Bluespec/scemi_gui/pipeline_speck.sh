e81286f0ce73385d947e56a01915e5037a5c6e70
Lauren K De Meyer <ldemeyer@mit.edu>
2016-05-04 16:13:33 -0400

    correct iv name in shell script

--- Bluespec/scemi_gui/pipeline_speck.sh
+++ Bluespec/scemi_gui/pipeline_speck.sh
@@ -5,7 +5,7 @@ cd fpga
 
 # define files
 key_file="key.txt"
-iv_file="key.txt"
+iv_file="iv.txt"
 message_input_file="in.txt"
 message_output_file="out.txt"
 
@@ -29,5 +29,5 @@ javac SpeckGUI.java #compile GUI
 #-- run GUI and tb --#
 while true; do #CTR-c CTR-c to kill; we'll add in user prompt in SpeckGUI
     java SpeckGUI $key_file $iv_file $message_input_file $message_output_file
-    runtb ./tb
+    #runtb ./tb
 done
